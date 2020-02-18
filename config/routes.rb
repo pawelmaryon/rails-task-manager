facd95f5b238fc192a574997d5326a1510b697a2
Pawel Maryon <maryonpawel@gmail.com>
2020-02-18 16:47:14 +0000

    task manager-2

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new'
   post "tasks", to: "tasks#create", as: :create
   get "tasks/:id", to: "tasks#show", as: :task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch '/tasks/:id', to: 'tasks#update', as: :update
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete
 end
